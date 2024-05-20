4df1f9617bda1f0f8f8988262367260894887dee
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-20 14:01:18 -0400

    Update tests

--- index.js
+++ index.js
@@ -40,22 +40,22 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
+  console.log("Word: racecar, Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
 
-  console.log("Expecting: true");
+  console.log("Word: kayak, Expecting: true");
   console.log("=>", isPalindrome("kayak"));
 
   console.log("");
 
-  console.log("Expecting: false");
+  console.log("Word: robot, Expecting: false");
   console.log("=>", isPalindrome("robot"));
 
   console.log("");
 
-  console.log("Expecting: false");
+  console.log("Word: paper, Expecting: false");
   console.log("=>", isPalindrome("paper"));
 }
 
